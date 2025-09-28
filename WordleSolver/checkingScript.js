0eec9561219bfd6c4e417714c1cda5c03ef57ae4
Ruby Leroux <ruby@mynameisruby.com>
2025-09-28 12:40:51 -0700

    added solver

--- WordleSolver/checkingScript.js
+++ WordleSolver/checkingScript.js
@@ -1,36 +1,66 @@
 var inputs = document.getElementsByClassName("letter");
-var output = document.getElementsByClassName("wordList");
+/* var output = document.getElementsByClassName("wordList"); */
 var allWords = new Array();
 
-var wordLength = 5;
+var wordLength = 8;
 allWords = getAllWords();
+const rules = document.styleSheets[0].cssRules;
 
-console.log(allWords.length);
-//console.log(text);
+updateLetterCount(5);
+
+var deviceIsMobile = false; //At the beginning we set this flag as false. If we can detect the device is a mobile device in the next line, then we set it as true.
+
+
+if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+    deviceIsMobile = true;
+
+}
+
+debug(navigator.userAgent);
+
+//const stylesheet = document.styleSheets[0];
+//console.log(stylesheet);
+
+
+/* for (let i=0; i<rules.length; i++) {
+    console.log(rules[i]);
+} */
+
 $(".search").on("click", function () {
     filterWords();
 });
 
+$(".clear").on("click", function () {
+    for (let i = 0; i < wordLength; i++) {
+        inputs[i].value = "";
+    }
+});
+
+$(".show-nolist").on("click", function () {
+    if ($('#no-list').css('display') == "none") {
+        $('#no-list').css('display', 'block');
+    } else {
+        $('#no-list').css('display', 'none');
+    }
+});
+
+
+
 
 function filterWords() {
-    var inputWord = new Array()
-    var constructedExpression = "";//"/^"
-    //inputs.forEach(element => {
-    for (let i = 0; i < inputs.length; i++) {
+    var constructedExpression = "";
+    for (let i = 0; i < wordLength; i++) {
         const thisLetter = inputs[i].value.toLowerCase();
-        console.log(thisLetter);
-        //inputWord.Add(element.value);
         if (thisLetter == "" || thisLetter == " ") {
             constructedExpression += "."
         } else {
             constructedExpression += "[" + thisLetter + "]";
         }
-        //});
     }
-    //constructedExpression += "$/";
     var isMatch = new RegExp(constructedExpression, "i");
 
-    console.log("regex: " + isMatch);
+    //console.log("regex: " + isMatch);
 
     var validWords = new Array();
 
@@ -41,64 +71,97 @@ function filterWords() {
 
         }
     }
-    console.log("valid words: " + validWords.length);
-    //output.validWords = allWords.toString();
-
-    document.getElementById('outputList').innerHTML = validWords.map((word) => {
-        return `<li>${word}</li>`;
+    //console.log("valid words: " + validWords.length);
+    document.getElementById('output_list').innerHTML = validWords.map((word) => {
+        return `<li class="output">${word}</li>`;
     }).join('');
+
+    $('li').click(function () {
+        console.log("clicked " + this.innerHTML);
+        if ($(this).hasClass("highlight")) {
+            $(this).removeClass('highlight');
+            //$(this).css('background_color', 'grey');
+        } else {
+            $(this).addClass('highlight');
+            //$(this).css('color', 'red');
+        }
+    });
 }
 
 function setAllWords() {
     allWords = new Array();
 }
 
-/* inputs.forEach(input => {
-    input.addEventListener("keyup", () => {
-        if (input.value.length === input.maxLength && parseInt(input.id) < inputLists.length) {
-
-            document.getElementById(parseInt(input.id) + 1).focus();
-            console.log("Advancing cursor");
-        }
-    })
-}) */
-
 for (let i = 0; i < inputs.length; i++) {
     const input = inputs[i];
-
     input.addEventListener("keyup", handleInput);
-    /* input.addEventListener("keyup", (event) => {
-        //console.log("letter entered");
-        console.log("key: "+KeyboardEvent.key);
-        //console.log(input.value.length+" =? "+input.maxLength);
-       // console.log("id int: "+parseInt(input.id));
-        if (input.value.length === input.maxLength && parseInt(input.id) < inputs.length) {
-
-            document.getElementById(parseInt(input.id) + 1).focus();
-            console.log("Advancing cursor");
-        }
-    }) */
-
 }
 
 function handleInput(e) {
-    console.log("key: " + e.code);
-    console.log("this: " + this);
-    document.getElementById("debug").innerHTML +="<li>key: " + e.code+"</li>";
+    // console.log("key: " + e.code);
+    // console.log("this: " + this);
+    //document.getElementById("debug").innerHTML += "<li>key: " + e.code + "</li>";
     const input = this;
-    //console.log(input.value.length+" =? "+input.maxLength);
-    // console.log("id int: "+parseInt(input.id));
 
-
-    if (e.code == "Backspace"&& parseInt(input.id) > 0 && input.value.length ===0) {
+    if (e.code == "Backspace" && parseInt(input.id) > 0 && input.value.length === 0) {
         document.getElementById(parseInt(input.id) - 1).focus();
-    } else if (e.code == "ArrowLeft"&& parseInt(input.id) > 0) {
+    } else if (e.code == "ArrowLeft" && parseInt(input.id) > 0) {
         document.getElementById(parseInt(input.id) - 1).focus();
-    } else if (e.code == "ArrowRight"&& parseInt(input.id) < inputs.length) {
+    } else if (e.code == "ArrowRight" && parseInt(input.id) < inputs.length) {
         document.getElementById(parseInt(input.id) + 1).focus();
     } else if (input.value.length === input.maxLength && parseInt(input.id) < inputs.length) {
 
         document.getElementById(parseInt(input.id) + 1).focus();
-        console.log("Advancing cursor");
+        //console.log("Advancing cursor");
     }
+}
+
+
+$(":input").on("input", function () {
+    console.log("radio button " + this.id + " selected: " + this.checked);
+    if (this.checked) {
+        var newLength = parseInt(this.value)
+        //console.log(newLength + " =? " + wordLength);
+        if (newLength != wordLength) {
+            updateLetterCount(newLength);
+        }
+    }
+});
+
+function updateLetterCount(newCount) {
+    /* $(".output").css('width', newCount+'em'); */
+
+
+    rules[0].style.width = newCount + "em";
+
+
+    wordLength = newCount;
+    for (let i = 0; i < inputs.length; i++) {
+        if (i < wordLength) {
+            $("#" + i).css('display', 'block');
+        } else {
+            $("#" + i).css('display', 'none');
+        }
+    }
+}
+
+
+
+$('li').click(function () {
+    console.log("clicked " + this.value);
+    if ($(this).hasClass("highlight")) {
+        $(this).removeClass('highlight');
+
+
+        //$(this).css('background_color', 'grey');
+    } else {
+        $(this).addClass('highlight');
+
+        //$(this).css('color', 'red');
+    }
+});
+
+function  debug(text) {
+    document.getElementById("debug").innerHTML += "<li>key: " + text + "</li>";
+    
 }
